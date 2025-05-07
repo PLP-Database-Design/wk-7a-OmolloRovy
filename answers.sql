4b13f7958a87204679f2a7a8f6f7b45f55f07b57
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2025-05-07 16:40:23 +0300

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,14 @@
+-- 
+
+SELECT
+    OrderID,
+    CustomerName,
+    TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(Products, ',', n), ',', -1)) AS Product
+FROM
+    ProductDetail
+CROSS JOIN
+    (SELECT 1 AS n UNION ALL SELECT 2 UNION ALL SELECT 3) AS numbers
+ON
+    LENGTH(Products) - LENGTH(REPLACE(Products, ',', '')) >= numbers.n - 1
+ORDER BY
+    OrderID;
